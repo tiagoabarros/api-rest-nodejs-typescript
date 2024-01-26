e898c51d134a3640c5b501fed63b3483c82a9a83
Tiago Barros <tiagoabarross@gmail.com>
2024-01-26 20:22:29 -0300

    feat: added yup validation with translated messages

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,14 +1,36 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface ICities {
-  name: string;
+  name: string
 }
 
-export const create = (req: Request<{}, {}, ICities>, res: Response) => {
+const bodyValidation: yup.ObjectSchema<ICities> = yup.object().shape({
+  name: yup.string().required().min(3)
+});
 
-  const data: ICities = req.body;
+export const create = async(req: Request<{}, {}, ICities>, res: Response) => {
+  let validatedData: ICities | undefined = undefined;
 
-  console.log(data);
+  try {
+    validatedData = await bodyValidation.validate(req.body, {abortEarly: false});
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach(err => {
+      if (!err.path) return;
+
+      errors[err.path] = err.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors
+    });
+  }
+
+  console.log(validatedData);
 
   return res.send("Created!");
 };
