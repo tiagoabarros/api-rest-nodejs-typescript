d7ad93feec552c46035fa0634453aafbf894c070
Tiago Barros <tiagoabarross@gmail.com>
2024-02-24 18:33:35 -0300

    feat: added tests to method create in cities controller

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -17,6 +17,6 @@ export const createValidation = validation((getSchema) => ({
 export const create = async(req: Request<{}, {}, ICities>, res: Response) => {
 
   console.log(req.body);
-
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Não Implementado!");
+  
+  return res.status(StatusCodes.CREATED).json(1);
 };
---
+++ tests/cities/Create.test.ts
@@ -0,0 +1,27 @@
+import { StatusCodes } from "http-status-codes";
+import { testServer } from "../jest.setup";
+
+describe("Cities Controller - Method Create", () => {
+  
+  it("Insert new city record", async () => {
+    const response = await testServer.post("/cidades").send( {"name": "Maceió"} );
+
+    expect(response.statusCode).toEqual(StatusCodes.CREATED);
+    expect(typeof response.body).toEqual("number");
+  });
+
+  it("Name of the new city record cannot be less than 3 characters long", async () => {
+    const response = await testServer.post("/cidades").send( {"name": "Ma"} );
+
+    expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(response.body).toHaveProperty("errors.body.name");
+  });
+
+  it("Name of new city record cannot be null", async () =>{
+    const response = await testServer.post("/cidades").send( {"name": ""} );
+
+    expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(response.body).toHaveProperty("errors.body.name");
+  });
+
+});
